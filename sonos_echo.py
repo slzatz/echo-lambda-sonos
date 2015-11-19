b18ceb86a2ca12df3fd9a905be80fbd79e8e2b67
slzatz <slzatz@gmail.com>
2015-11-19 07:21:42 -0500

    sonos_echo.py now uses dynamo query to get data on the current song that is playing

--- sonos_echo.py
+++ sonos_echo.py
@@ -1,6 +1,7 @@
 '''
 Radio play {neil young|artist} radio
 Shuffle shuffle {number} {neil young|artist} songs
+Select {myartist}
 Deborah play {number} of Deborah's albums
 WhatIsPlaying what is playing now
 WhatIsPlaying what song is playing now
@@ -13,7 +14,7 @@ TurnTheVolume Turn the volume {volume}
 TurnTheVolume Turn {volume} the volume
 '''
 import boto3
-from boto3.dynamodb.conditions import Attr
+from boto3.dynamodb.conditions import Attr, Key
 import json
 from decimal import Decimal
 from time import time
@@ -123,33 +124,17 @@ def intent_request(session, request):
     elif intent == "WhatIsPlaying":
 
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        table = dynamodb.Table('scrobble')
-        z = time()
-        #>>> r = table.scan(Limit=10, FilterExpression=Attr("ts").gt(Decimal(z)-300))
-        #>>> r
-        #{u'Count': 1, u'Items': [{u'album': u'I Carry Your Heart With Me (c)', u'artist': u'Hem', u'title': u'The Part Where You Let Go', 
-        #u'ts': Decimal('1445364875'), u'date': u'2007 - Home Again, Home Again', u'scrobble': u'27'}], 
-        #u'LastEvaluatedKey': {u'ts': Decimal('1442178047'), u'artist': u'Leo Kottke'}, 
-        #u'ScannedCount': 10, 'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': 'P3U632LF4NKTGP6MEJ228MLRDBVV4KQNSO5AEMVJF66Q9ASUAAJG'}}
-        # if not result found
-        #{u'Count': 0, u'Items': [], u'LastEvaluatedKey': {u'ts': Decimal('1442178047'), u'artist': u'Leo Kottke'}, u'ScannedCount': 10, 
-        #'ResponseMetadata': {'HTTPStatusCode': 200, 'RequestId': '2UVLSDD8147256OV6P0T03IBV7VV4KQNSO5AEMVJF66Q9ASUAAJG'}}
-
-        result = table.scan(FilterExpression=Attr('ts').gt(Decimal(z)-300)) #300 seconds since song will have started and been posted prior to request
+        table = dynamodb.Table('scrobble_new')
+        result = table.query(KeyConditionExpression=Key('location').eq('nyc'), ScanIndexForward=False, Limit=1) #by default the sort order is ascending
 
         if result['Count']:
-            songs = result['Items']
-            y = [(s.get('ts', ''), s.get('title', ''), s.get('artist', ''), s.get('album', '')) for s in songs]
-            y.sort(key = lambda x:x[0], reverse=True)
-            
-            for x in y:
-                print "{}: {} - {} - {}".format(datetime.fromtimestamp(x[0]).strftime("%a %I:%M%p"), x[1], x[2], x[3])
-
-            last_song = y[0]
-            output_speech = "The song is {}. The artist is {} and the album is {}.".format(last_song[1], last_song[2], last_song[3])
-
+            track = result['Items'][0]
+            if track['ts'] > Decimal(time.time())-300:
+                output_speech = "The song is {}. The artist is {} and the album is {}.".format(track.get('title','No title'), track.get('artist', 'No artist'), track.get('album', 'No album'))
+            else:
+                output_speech = "Nothing appears to be playing right now, Steve"
         else:
-            output_speech = "Nothing appears to be playing right now, Steve"
+            output_speech = "It appears that nothing has ever been scrobbled"
 
         output_type = 'PlainText'
         response = {'outputSpeech': {'type':output_type,'text':output_speech},'shouldEndSession':True}
@@ -157,9 +142,6 @@ def intent_request(session, request):
 
     elif intent == "Skip":
 
-        #sqs = boto3.resource('sqs')
-        #queue = sqs.get_queue_by_name(QueueName='echo_sonos')
-        #sqs_response = queue.send_message(MessageBody=json.dumps({"action":"skip"}))
         send_sqs(action='skip')
 
         output_speech = "skipped"
