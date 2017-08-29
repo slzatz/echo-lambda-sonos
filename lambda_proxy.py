3938720221cc86a8791be583f12e3463ced74f2e
slzatz <slzatz@gmail.com>
2017-08-29 07:03:38 -0400

    lambda_proxy.py: cleanup of docstring

--- lambda_proxy.py
+++ lambda_proxy.py
@@ -1,30 +1,9 @@
 '''
-var http = require('http'); 
-var URLParser = require('url'); 
-exports.handler = function (json, context)
-
-          { try 
-          
-          { // A list of URL's to call for each applicationId
-
-     var handlers = { 'appId':'url', 'amzn1.echo-sdk-ams.app.999999-d0ed-9999-ad00-999999d00ebe':'http://alexa-app.com/helloworld' }; // Look up the url to call based on the appId 
-     var url = handlers[json.session.application.applicationId]; 
-
-if (!url) { context.fail("No url found for application id"); } 
-
-var parts = URLParser.parse(url); 
-var post_data = JSON.stringify(json); // An object of options to indicate where to post to 
-var post_options = { host: parts.hostname, port: (parts.port || 80), path: parts.path, method: 'POST', headers: { 'Content-Type': 'application/json', 'Content-Length': post_data.length } 
-}; // Initiate the request to the HTTP endpoint 
-
-var req = http.request(post_options,function(res) { var body = ""; // Data may be chunked res.on('data', function(chunk) { body += chunk; }); res.on('end', function() { // When data is 
-done, finish the request context.succeed(JSON.parse(body)); }); }); req.on('error', function(e) { context.fail('problem with request: ' + e.message); }); // Send the JSON data req.write
-(post_data); req.end(); }
-
-catch (e) { context.fail("Exception: " + e); }
-
-}; 
+Lambda script to respond to a Sonos echo command and direct the action to the right local
+raspberry pi in nyc or ct
+Also directly addresses a request to get and set the location.
 '''
+
 import requests
 import boto3
 import json
@@ -95,6 +74,7 @@ def intent_request(event, request):
         obj = s3.Object('sonos-scrobble','location')
         location = obj.get()['Body'].read()
         url = urls[location]
+        print "url =", url
         try:
             response = requests.post(url, json=event)
         except Exception as e:
